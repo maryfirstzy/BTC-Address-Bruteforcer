67646b1b86b2b2e7e93ccd023be9d6883cd86ead
maryfirstzy <maryfirst865@gmail.com>
2026-07-17 11:19:09 -0400

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -8,9 +8,7 @@ i = 0
 
 while True:
 
-    wallet = bitcoinlib.Wallet()
-
-
+    wallet = bitcoinlib.wallets.Wallet.create('WalletName')
     i += 1
     print("[{}] {}".format(i, wallet.get_address_uncompressed()))
     if wallet.get_address_uncompressed() == address_to_hack:
